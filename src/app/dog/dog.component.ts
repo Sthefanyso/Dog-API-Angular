5fb8bf0630835de180f022424fa3f6a160b4ff2c
Sthefanyso <sthefany.silva3@fatec.sp.gov.br>
2023-04-18 11:27:17 -0300

    Ultimo Commit

--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DogService } from './../dog.service';
+import { Component, OnInit } from '@angular/core';
 import { Dog } from '../dog';
 
 @Component({
@@ -6,12 +7,29 @@ import { Dog } from '../dog';
   templateUrl: './dog.component.html',
   styleUrls: ['./dog.component.css']
 })
-export class DogComponent {
-  dog: Dog = {"message":"https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_612.jpg",
-               "status": "true"
-              } as Dog;
+export class DogComponent implements OnInit {
 
-        getName(): string[]{
-          return this.dog.message.split("/")[4].split("-");
-        }
+  constructor(private DogService: DogService){ }
+
+  ngOnInit(): void {
+    this.loadDog();
+  }
+  loadDog() {
+    this.DogService.getRandomDog().subscribe(
+      {
+        next: dog => this.dog = dog
+      }
+    );
+  }
+
+  dog: Dog = {} as Dog;
+
+   getName(): string[]{
+      return this.dog.message.split("/")[4].split("-");
+    }
+
 }
+function loadDog() {
+  throw new Error('Function not implemented.');
+}
+
